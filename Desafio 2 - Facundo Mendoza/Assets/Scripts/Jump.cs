be60e3de6e4f8c6420cb94f6b6500c0d6404e575
FacuM0923 <facumendoza879@gmail.com>
2024-09-06 20:09:59 -0300

    Se incorporaron nuevos elementos al proyecto como: Animacion, música y efectos de sonido

--- Desafio 2 - Facundo Mendoza/Assets/Scripts/Jump.cs
+++ Desafio 2 - Facundo Mendoza/Assets/Scripts/Jump.cs
@@ -8,16 +8,21 @@ public class Jump : MonoBehaviour {
     [Header("Configuracion")]
     [SerializeField] private float strength_jump = 5f;
 
+    [SerializeField] private AudioClip jumpSFX;
+    [SerializeField] private AudioClip collisionSFX;
+
     //Variables de uso interno en el script
     private bool can_jump = true;
     private bool jumping = false;
 
     // Variable para referenciar otro componente del objeto
     private Rigidbody2D rb2D;
+    private AudioSource audio;
 
     // Codigo ejecutado cuando el objeto se activa en el nivel
     private void OnEnable(){
         rb2D = GetComponent<Rigidbody2D>();
+        audio = GetComponent<AudioSource>();
     }
 
     //Codigo ejecutado en cada frame del juego (Intervalo variable)
@@ -25,14 +30,20 @@ public class Jump : MonoBehaviour {
 
         if (Input.GetKeyDown(KeyCode.Space) && can_jump){
             can_jump = false;
-        }
 
+            if (audio.isPlaying) { return; }
+            audio.PlayOneShot(jumpSFX);
+
+        }
     }
 
     private void FixedUpdate(){
+
         if (!can_jump && !jumping){
+
             rb2D.AddForce(Vector2.up * strength_jump, ForceMode2D.Impulse);
             jumping = true;
+
         }
     }
 
@@ -40,6 +51,9 @@ public class Jump : MonoBehaviour {
     private void OnCollisionEnter2D(Collision2D collision){
         can_jump = true;
         jumping = false;
-    }
 
+        if (audio.isPlaying){ return; }
+        audio.PlayOneShot(collisionSFX);
+
+    }
 }
