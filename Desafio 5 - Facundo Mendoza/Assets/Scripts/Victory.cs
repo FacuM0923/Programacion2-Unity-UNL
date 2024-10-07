9fd7e89caba4274995a4a9524083410e8b4769d0
FacuM0923 <facumendoza879@gmail.com>
2024-10-07 17:38:45 -0300

    Nuevo commit
    Se incorporó un nuevo proyecto al repositorio, el cuál es denominado Desafío 5.

---
+++ Desafio 5 - Facundo Mendoza/Assets/Scripts/Victory.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class Victory : MonoBehaviour{
+
+    public GameObject VictoryPanel;
+
+    public void ShowVictory(){
+        
+        VictoryPanel.SetActive(true);
+
+    }
+
+}
